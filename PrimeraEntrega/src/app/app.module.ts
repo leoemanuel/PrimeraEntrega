5e38e4fd1cd061c9b238ec34e4c9c8a436d2c5df
le0h <xploit.ghost@gmail.com>
2022-05-08 04:06:21 -0300

    Primera Entrega

--- PrimeraEntrega/src/app/app.module.ts
+++ PrimeraEntrega/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidemenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
